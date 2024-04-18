7038c6390b516283895f39c746878399df55a10c
shahid <shahidhashmiraj@gmail.com>
2024-04-18 21:28:16 +0530

    metrics

---
+++ Metrics & tools/Metrics.js
@@ -0,0 +1,37 @@
+//  We can't improve what we can't measure
+
+/*
+There are two type of matrics 
+
+1. Browser-Centric Metrics - Time to first byte, Load.
+2. User-Centric Metrics - Content layout Shift, Largest Contentful Paint.
+
+ * Browser-centric metrics
+    Time to first byte
+        The browser to receive the first byte from the HTTP response from the server.
+        'when we click on link it's start from 0 and the server will do something and give a response like sending a index file first byte this calculated'
+
+    First Paint
+        tells us the has paint something in timeline  but that something can be a line or background color.
+
+    Page Load
+        All the resources are downloaded and everything is painted on the screen.
+    
+==================================================================================================================================================
+    *User-centric metrics 
+       1.  First Interactive
+            means main thresd has been realease 
+
+        2. Speed index
+            final render at everypoint 
+        
+        3. Time to Interactive
+            measures how long it takes for a web page to become fully interactive for the user.
+
+        4. Largest contentful Paint (LCP)
+*IMP        means when your largest content is painted it could be any thing img
+
+        5. custom Metrics
+            create your own 
+
+*/  
